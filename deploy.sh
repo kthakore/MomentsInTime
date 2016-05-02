e0472933ad9c30a410e84465fced0e99a59f6ecc
Kartik Thakore <thakore.kartik@gmail.com>
2016-05-01 21:47:24 -0400

    rebuilding site Sun  1 May 2016 21:47:24 EDT

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-#node_modules/.bin/webpack --progress -p 
+node_modules/.bin/webpack --progress -p 
 
 # Add changes to git.
 git add -A
