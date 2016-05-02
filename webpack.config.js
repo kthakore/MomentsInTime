4048bc2636ef87638e1ba70352fc4a4cc5fdfa31
Kartik Thakore <thakore.kartik@gmail.com>
2016-05-01 20:35:55 -0400

    Oops wrong port

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path'),
     webpack  = require('webpack');
 module.exports = {
     entry: [
-        'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+        'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
         'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
         './src/entry.js'
     ],
